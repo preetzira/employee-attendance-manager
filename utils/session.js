63005ceb63b9708592d0f370f99adf43eff2bee6
Gurpreet Singh <preetzira08@gmail.com>
2021-05-15 20:13:53 +0530

    refactor: jwt sign and verify methods using public/private keys with passphrase in utils/session.js

--- utils/session.js
+++ utils/session.js
@@ -1,26 +1,52 @@
+const fs = require("fs")
+const path = require("path")
 const jwt = require("jsonwebtoken")
 const Sessions = require("../models/sessions")
+const jwtPublicKey = fs.readFileSync(
+  path.join(__dirname, "../keys/jwt-public.key"),
+  "utf-8",
+)
+const jwtPrivateKey = fs.readFileSync(
+  path.join(__dirname, "../keys/jwt-private.key"),
+  "utf-8",
+)
 
 class AuthSession {
   constructor(sid, exp) {
     this.sid = sid
     this.exp = exp
+    this.jwtOptions = {
+      subject: process.env.JWT_SUB,
+      issuer: process.env.JWT_ISS,
+      audience: process.env.JWT_AUD,
+    }
+    this.jwtSignOptions = {
+      ...this.jwtOptions,
+      expiresIn: +((new Date(exp) - new Date()) / 1000).toFixed(0),
+      algorithm: "RS256",
+    }
+    this.jwtVerifyOptions = {
+      ...this.jwtOptions,
+      algorithms: ["RS256"],
+    }
   }
   toJson() {
-    return { sid: this.sid, exp: +(new Date(this.exp) / 1000).toFixed(0) }
+    return { sid: this.sid }
   }
   encoded() {
     return jwt.sign(
       {
         ...this.toJson(),
       },
-      process.env.SECRET_KEY,
+      { key: jwtPrivateKey, passphrase: process.env.JWT_KEY_PASS },
+      this.jwtSignOptions,
     )
   }
   static async decoded(decodedJwt) {
     return jwt.verify(
       decodedJwt,
-      process.env.SECRET_KEY,
+      jwtPublicKey,
+      this.jwtVerifyOptions,
       async (error, res) => {
         if (error) throw new Error(error.message)
         return await Sessions.findOne(
@@ -38,7 +64,8 @@ class AuthSession {
   static async destroy(decodedJwt) {
     return jwt.verify(
       decodedJwt,
-      process.env.SECRET_KEY,
+      jwtPublicKey,
+      this.jwtVerifyOptions,
       async (error, res) => {
         if (error) throw new Error(error.message)
         return await Sessions.findByIdAndRemove(res.sid)
@@ -54,6 +81,7 @@ const createSession = ({ req, res, user, onModel }) => {
     deviceOs: req.header("deviceOs"),
     ipAddress: req.ip,
     location: req.header("location"),
+    expiresAt: new Date(Date.now() + +process.env.TOKEN_EXPIRATION_TIME),
   }).then((session) => {
     const authSession = new AuthSession(session._id, session.expiresAt)
     const token = authSession.encoded()
