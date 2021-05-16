3c8e983db83ad92cc78c34121dd1e54aa6f0dbe4
Gurpreet Singh <preetzira08@gmail.com>
2021-05-16 10:39:25 +0530

    chore: added 'verbose' option in 'jest.config.js'

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   rootDir: "./",
   setupFilesAfterEnv: ["<rootDir>/tests/config/setup.js"],
+  verbose: true,
   // globalSetup: "<rootDir>/tests/config/setup.js", doesn't work with mongoose
   // globalTeardown: "<rootDir>/tests/config/teardown.js", doesn't work with mongoose
   testEnvironment: "node",
